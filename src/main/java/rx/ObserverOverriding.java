e5a78b28902268db5158fa059c560046aa05d176
pyoung <pyoungx7@gmail.com>
2018-06-02 03:05:44 -0400

    observer overriding lambdas

--- src/main/java/rx/ObserverOverriding.java
+++ src/main/java/rx/ObserverOverriding.java
@@ -3,6 +3,9 @@ package rx;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+
 
 public class ObserverOverriding {
     public static void main(String[] args) {
@@ -39,5 +42,27 @@ public class ObserverOverriding {
 
         numbers.map(number -> "" + number / 0)
                 .subscribe(observer);
+
+
+        System.out.println("----------- Lambda Version -----------------------------------");
+
+        // kind of wondering why they didn't use the jdk18 consumer and just a regular Runnable
+        // instead of a pseudo Runnable called Action.. wouldn't that make things more flexible???
+        final Consumer<String> onNextFunction = System.out::println;
+        final Action onCompleteFunction = () -> System.out.println("Observable's emissions are all done");
+        final Consumer<Throwable> onError = throwable -> System.out.println("oops an error happened " + throwable);
+
+        // recall from earlier how we implemented something similar to
+        /*
+                A more complete subscribe call example looks like this:
+        def myOnNext     = { item ->  do something useful with item  };
+        def myError      = { throwable ->  react sensibly to a failed call  };
+        def myComplete   = { clean up after the final response  };
+        def myObservable = someMethod(itsParameters);
+        myObservable.subscribe(myOnNext, myError, myComplete);
+        - http://reactivex.io/documentation/observable.html
+         */
+
+        numbers.map(number -> "" + number).subscribe(onNextFunction, onError, onCompleteFunction);
     }
 }
